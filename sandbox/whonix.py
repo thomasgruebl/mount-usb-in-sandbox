1e4dba48087566523cc3e9fc309a3fb06597cb56
thomasgruebl <thomasgruebl@gmx.at>
2021-11-16 00:37:32 +0800

    adding whonix file

---
+++ sandbox/whonix.py
@@ -0,0 +1,10 @@
+import core
+
+
+class Whonix(Sandbox):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def mount_usb_to_sandbox(self, usb_uuid: list[str]):
+        # probably needs more sophisticated logic to mount usb (than parent method for "normal" VM)
+        pass
