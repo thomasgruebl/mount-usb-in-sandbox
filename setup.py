f9a46d0ad1d2930a8050592c241d9d73b78d5ac1
thomasgruebl <thomasgruebl@gmx.at>
2021-11-16 14:43:21 +0800

    add first version of setup

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='mount-usb-in-sandbox',
+    version='1.0.0',
+    description='Automatically mounts a USB device in a sandbox',
+    long_description=long_description,
+    author='Thomas Gruebl',
+    url='https://github.com/thomasgruebl/mount-usb-in-sandbox',
+    packages=['usb', 'sandbox'],
+    license='MIT',
+    keywords='usb mount sandbox whonix',
+    python_requires='>=3.8'
+)
