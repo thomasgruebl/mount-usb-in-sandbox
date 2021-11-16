642d0bf065c191781927d3b84f92999008655941
thomasgruebl <thomasgruebl@gmx.at>
2021-11-16 14:42:49 +0800

    add typing

--- sandbox/whonix.py
+++ sandbox/whonix.py
@@ -1,10 +1,11 @@
 from sandbox.core import Sandbox
+from typing import List
 
 
 class Whonix(Sandbox):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def mount_usb_to_sandbox(self, usb_uuid: list[str]):
+    def mount_usb_to_sandbox(self, usb_uuid: List[str]):
         # probably needs more sophisticated logic to mount usb (than parent method for "normal" VM)
         pass
