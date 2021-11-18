e578c7042db842f33b4c26f72cf253c48a1d38b7
thomasgruebl <thomasgruebl@gmx.at>
2021-11-18 18:34:59 +0800

    updating class

--- sandbox/whonix.py
+++ sandbox/whonix.py
@@ -1,11 +1,6 @@
 from sandbox.core import Sandbox
-from typing import List
 
 
 class Whonix(Sandbox):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-
-    def mount_usb_to_sandbox(self, usb_uuid: List[str]):
-        # probably needs more sophisticated logic to mount usb (than parent method for "normal" VM)
-        pass
